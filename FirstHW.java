b118908df2561374bc3b8a5a58d47dead03abfd4
marowanrice <marowanrice@gmail.com>
2020-03-01 10:56:38 -0500

    Add files via upload

---
+++ FirstHW.java
@@ -0,0 +1,34 @@
+// FirstHW.java
+// Rowan Rice (mar1185)
+// September 9, 2019
+
+// This file prints out the lyrics 
+// to a song in accordance with 
+// homework assignment #1.  
+
+public class FirstHW 
+{
+	public static void main(String [] args) 
+	{	
+		// print a blank line before title
+		System.out.println("\nMaria\t by Richard Rodgers\n"); 
+		// print a double line after title
+
+		// print first verse using escape characters
+		System.out.print("How do you solve a problem like Maria?\n");
+		System.out.print("How do you catch a cloud a pin it down?\n");
+		System.out.print("How do you find a word that means Maria?\n");
+		System.out.println("A flibbertigibbet! A will-o'-the-wisp! A CLOWN\n");
+
+		// print second verse using println
+		System.out.println("Many a thing you know you'd like to tell her");
+		System.out.println("Many a thing she ought to understand");
+		System.out.println("But how do you make her stay");
+		System.out.println("And listen to all you say?");
+		System.out.println("How do you keep a wave upon the sand?\n");
+
+		System.out.println("Oh, how do you solve a problem like Mariaaaaa?");
+		System.out.println("How do you hold a moonbeam in your hand?!\n");
+		//print final blank line
+	}
+}
